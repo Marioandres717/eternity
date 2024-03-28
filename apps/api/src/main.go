9a319561ab4ff8c63080730ea190dc9b5daeed3c
Mario Andres <marioarendon2@gmail.com>
2024-03-28 02:07:19 -0600

    add echo as go server framework

--- apps/api/src/main.go
+++ apps/api/src/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	// code here
-	fmt.Println("Hello, World!")
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
